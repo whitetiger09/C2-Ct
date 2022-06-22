d516a12e06c18176ff6746d51644963b6690be13
Darshan0707 <darshanc@sjcem.edu.in>
2022-06-22 09:30:05 +0530

    2nd commit

---
+++ C2-Tc/src/pack2/B.java
@@ -0,0 +1,12 @@
+package pack2;
+import pack1.*;
+
+public class B {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         A t=new A();
+         t.msg();
+	}
+
+}
---
+++ C2-Tc/src/pack3/C.java
@@ -0,0 +1,12 @@
+package pack3;
+import pack1.A;
+
+public class C {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        A t=A();
+        t.msg();
+	}
+
+}
