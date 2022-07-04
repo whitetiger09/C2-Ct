5c849e8ecda57d1e69ae461147d0f9c6fca1f4be
Darshan0707 <darshanc@sjcem.edu.in>
2022-07-04 15:59:28 +0530

    constructor commited

---
+++ C2-Tc/src/com/tns/constructor/Constructor.java
@@ -0,0 +1,19 @@
+package com.tns.constructor;
+
+public class Constructor {
+	public Constructor()
+	{
+	System.out.println("Constructor is invoked");
+	}
+		public static void main(String args[])
+		{
+			Constructor s=new Constructor();
+			Constructor s1=new Constructor();
+			Constructor s2=new Constructor();
+			Constructor s3=new Constructor();
+			
+		}
+		
+	}
+
+
