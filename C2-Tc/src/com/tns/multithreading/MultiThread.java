be3d977dcf97441e7cfa8b5b7f2fbfec71c923e5
Darshan0707 <darshanc@sjcem.edu.in>
2022-08-13 15:02:52 +0530

    d

---
+++ C2-Tc/src/com/tns/multithreading/MultiThread.java
@@ -0,0 +1,56 @@
+package com.tns.multithreading;
+
+
+
+//Using Extends
+
+public class MultiThread {
+
+	public static void main(String[] args) {
+		MyThread t=new MyThread();
+		t.start();
+		for(int i=0; i<10; i++)
+		{
+			System.out.println("Main Thread.....");
+		}
+
+	}
+
+}
+class MyThread extends Thread
+{
+	public void run()
+	{
+		for(int i=0; i<10; i++)
+		{
+			System.out.println("child Thread.....");
+		}
+	}
+}
+
+//Using runnable interface
+
+//public class Multi_Thread {
+//
+//	public static void main(String[] args) {
+//		MyThread t=new MyThread();
+//		Thread obj=new Thread(t);
+//		obj.start();
+//		for(int i=0; i<10; i++)
+//		{
+//			System.out.println("Main Thread.....");
+//		}
+//
+//	}
+//
+//}
+//class MyThread implements Runnable
+//{
+//	public void run()
+//	{
+//		for(int i=0; i<10; i++)
+//		{
+//			System.out.println("child Thread.....");
+//		}
+//	}
+//}
