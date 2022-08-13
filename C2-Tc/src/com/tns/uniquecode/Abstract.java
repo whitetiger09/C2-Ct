b97cc101e0979d64fcfdd8ae849b5c02c05e8453
Darshan0707 <darshanc@sjcem.edu.in>
2022-08-13 15:31:31 +0530

    OK

---
+++ C2-Tc/src/com/tns/uniquecode/Abstract.java
@@ -0,0 +1,23 @@
+package com.tns.uniquecode;
+
+
+
+public class Abstract {
+
+	public static void main(String[] args) {
+		Scooty Activa=new Scooty();
+		Activa.noofwheels();
+
+	}
+
+}
+abstract class Vehicle
+{
+	abstract public void noofwheels();
+}
+class Scooty extends Vehicle
+{
+	public void noofwheels() {
+		System.out.println("No of wheels in scooty=" +2);
+	}
+}
